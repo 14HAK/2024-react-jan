3b20f70f4c5841a64b383719131ee8acbfedea14
Dulon <dulonmolla88@gmail.com>
2024-01-20 00:34:16 +0600

    useContext HOOK project, data shere to any components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { createContext } from 'react';
 import Home from './components/Home';
 import Manu from './components/Manu';
 
+export const myContext = createContext();
+
 function App() {
+  const myContextData = {
+    entities: [
+      { name: 'jonas', age: 32 },
+      { name: 'dulon', age: 28 },
+    ],
+    loading: 'idle',
+  };
+
   return (
-    <>
+    <myContext.Provider value={myContextData}>
       <Home />
       <Manu />
-    </>
+    </myContext.Provider>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,9 @@
+import { useContext } from 'react';
+import { myContext } from '../App';
+
 function Home() {
+  const data = useContext(myContext);
+  console.log(data);
   return (
     <div>
       <p className='text-5xl font-bold px-10 py-10 text-green-700'>HOME PAGE</p>
