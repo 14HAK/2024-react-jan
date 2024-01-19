fc685a87ff1eb07f28854e5ac05b6a56c756cdc3
Dulon <dulonmolla88@gmail.com>
2024-01-20 00:25:33 +0600

    demo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,13 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import AppLayout from './components/AppLayout';
 import Home from './components/Home';
-import Cart from './components/Cart';
 import Manu from './components/Manu';
-import Posts from './components/Posts';
-import { getPostData } from './fetures/getPost';
-import CreatePost from './components/CreatePost';
-import Search from './components/Search';
-
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-        errorElement: <p>error in home router</p>,
-      },
-      {
-        path: '/manu',
-        element: <Manu />,
-        errorElement: <p>error in home router</p>,
-      },
-      {
-        path: '/cart',
-        element: <Cart />,
-        errorElement: <p>error in home router</p>,
-      },
-      {
-        path: '/posts',
-        element: <Posts />,
-        errorElement: <p>error in home router</p>,
-        loader: () => getPostData(),
-      },
-      {
-        path: '/newpost',
-        element: <CreatePost />,
-        errorElement: <p>error in home router</p>,
-        // action: createSinglePost,
-      },
-      {
-        path: '/search',
-        element: <Search />,
-        errorElement: <p>error in home router</p>,
-        // action: createSinglePost,
-      },
-    ],
-  },
-  {
-    path: '*',
-    element: <p> global error happen!</p>,
-  },
-]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Home />
+      <Manu />
+    </>
+  );
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,4 @@
 function Home() {
-  // const fetcher = useFetcher();
-  // console.log(fetcher);
-
   return (
     <div>
       <p className='text-5xl font-bold px-10 py-10 text-green-700'>HOME PAGE</p>
